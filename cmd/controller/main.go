41a2e70404475c2664e08bd7b3539921a79c311f
joey <zhengcheng.zheng@alibaba-inc.com>
2022-09-09 00:13:05 +0800

    add controller.yaml

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/chengjoey/pipelines/pkg/reconciler/pipelinerun"
 	"github.com/chengjoey/pipelines/pkg/reconciler/taskrun"
 	"knative.dev/pkg/injection/sharedmain"
 )
 
 func main() {
+	// sets up liveness and readiness probes.
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handler)
+	mux.HandleFunc("/health", handler)
+	mux.HandleFunc("/readiness", handler)
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":8080", mux))
+	}()
 	sharedmain.Main("controller",
 		taskrun.NewController,
 		pipelinerun.NewController,
 	)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
